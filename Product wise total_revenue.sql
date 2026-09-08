bcbeb8e6158ffd7a3fc7c204c516126c4055b05f
sharmatushar0610-droid <sharmatushar0610@gmail.com>
2026-09-08 12:37:51 +0530

    Create Product wise total revenue.sql

---
+++ Product wise total_revenue.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.product_name,
+    SUM(s.quantity) AS total_qty,
+    SUM(s.quantity * CAST(REPLACE(REPLACE(p.unit_price_usd, '$', ''), ',', '') AS NUMERIC)) AS total_revenue_usd
+FROM sales s
+JOIN products p ON s.productkey = p.productkey
+GROUP BY p.product_name
+ORDER BY total_revenue_usd DESC
+LIMIT 10;
